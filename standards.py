bcc969948e05aee2544f03354aa2028a4a7fa69d
ddgallichroum <didi@didi.com.au>
2016-11-09 13:04:41 +0800

    all works bar saving to a file

--- standards.py
+++ standards.py
@@ -1,21 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 
-# Opening from the web using requests module
-fhandle = requests.get('http://www.jazzstandards.com/compositions/index4.htm')
-fhandle.raise_for_status()
-soup = BeautifulSoup(fhandle.text, 'html.parser')
+# The base url:
+baseurl = 'http://www.jazzstandards.com/compositions/'
 
+url = baseurl + 'index.htm'
 
-# Extract the <tr class='JSontentsLine'> tags
-trsongs = soup('tr','JSContentsLine')
+while True:
+    # Open the url and soupize.
+    fhandle = requests.get(url)
+    fhandle.raise_for_status()
+    soup = BeautifulSoup(fhandle.text, 'html.parser')
 
-# Loop through these <tr>s and extract the rank, year and title
-# Note that title text is trickier to extract, because it sits inside a <a> tag
-for trsong in trsongs:
-    rank = unicode(trsong.contents[0].contents[0])
-    year = unicode(trsong.contents[1].contents[0])
-    title = unicode(trsong.contents[2].contents[1].contents[0])
-    print rank + ',' + year + ',' + title
+    # Songs are in a <tr class='JSContentsLine'> tag, so extract these.
+    trsongs = soup('tr','JSContentsLine')
 
+    # For each song, extract the rank, year and title as a tab-separated line.
+    # Note that the title sits in an <a> tag, so need to go in deeper to extract.
+    for trsong in trsongs:
+        rank = unicode(trsong.contents[0].contents[0])
+        year = unicode(trsong.contents[1].contents[0])
+        title = unicode(trsong.contents[2].contents[1].contents[0])
+        print rank + '\t' + year + '\t' + title
 
+    # The 'next' url (if exists) is in a solitary <p align="right"> so find this.
+    nextlinkp = soup.find('p',align="right")
+
+    # Test 'next' url, break if invalid (i.e. tag has no name), reset url if valid.
+    if nextlinkp.contents[2].name == None:
+        break
+    else:
+        url = baseurl + unicode(nextlinkp.contents[2]['href'])
+
+print 'Done here'
