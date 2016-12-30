aba0cdbbc41c91d3116cd939d0a453673aa97fdb
ddgallichroum <didi@didi.com.au>
2016-12-30 17:45:49 +0800

    cleaned it up

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 #Jazz Standards Extractor
 
-Motivation: to extract as a .csv (or whatever) file, the list of the top 1000 jazz standards on [http://jazzstandards.com].
+This `standards.py` Python script is a simple web scraper that goes to [jazzstandards.com](http://jazzstandards.com) and extracts the rank, title and year of the 1000 most popular jazz standards (as calculated by the website author's [methodology](http://www.jazzstandards.com/overview.ranking.htm)).
 
-[This](http://www.jazzstandards.com/compositions/index.htm) is the first (of 10) pages of the site that contains what we want.
+[This](http://www.jazzstandards.com/compositions/index.htm) is the first page (of 10) of the site that contains the information we wish to extract. As you can see, the data is in a straightforward HTML table. This script uses [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/), make sure it is installed before you run it.
 
-The data is in a straightforward HTML table, we will extract it with Beautiful Soup 4.
+Usage:
 
-So far, we can get csv out of a downloaded version of this first page.
+    python standards.py
 
-Next steps:
+Or if you prefer to save the output to a file (which is most likely the case):
 
-* to get it straight off the web (Done 08.11.2016)
-* to navigate to the next 9 pages, and extract each page
+    python standards.py > output.txt
 
--- standards.py
+++ standards.py
@@ -1,32 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 
-# The base url:
+# The base url
 baseurl = 'http://www.jazzstandards.com/compositions/'
 
+# This is the url for the first 100 songs
 url = baseurl + 'index.htm'
 
 while True:
-    # Open the url and soupize.
+    # Open the url for the first 100 songs, and soupize
     fhandle = requests.get(url)
     fhandle.raise_for_status()
     soup = BeautifulSoup(fhandle.text, 'html.parser')
 
-    # Songs are in a <tr class='JSContentsLine'> tag, so extract these.
+    # Songs are in a <tr class='JSContentsLine'> tag, so extract these tags
     trsongs = soup('tr','JSContentsLine')
 
-    # For each song, extract the rank, year and title as a tab-separated line.
-    # Note that the title sits in an <a> tag, so need to go in deeper to extract.
+    # For each song, extract the rank, year and title as a tab-separated line
+    # Note that the title sits in an <a> tag, so we need to go in deeper to extract
     for trsong in trsongs:
         rank = unicode(trsong.contents[0].contents[0])
         year = unicode(trsong.contents[1].contents[0])
         title = unicode(trsong.contents[2].contents[1].contents[0])
         print rank + '\t' + year + '\t' + title
 
-    # The 'next' url (if exists) is in a solitary <p align="right"> so find this.
+    # The 'next' url to load the next page of 100 standards (if exists)
+    # is in a solitary <p align="right"> so find this
     nextlinkp = soup.find('p',align="right")
 
-    # Test 'next' url, break if invalid (i.e. tag has no name), reset url if valid.
+    # Test for existence of 'next' url, break if invalid (i.e. tag has no name),
+    # reset url if valid
     if nextlinkp.contents[2].name == None:
         break
     else:
